0b4c6111c03b960994663050f80b7ccaecf6787f
Kartheek Maremalla <kmaremalla@DW20051USSLM.local>
2016-04-22 00:18:09 -0600

    removed some sample code that was added at the start of project

--- public/js/angularApp.js
+++ public/js/angularApp.js
@@ -1,9 +1,4 @@
 var app = angular.module('profile', ['profileControllers','ngRoute','appRoutes']);
 
 
-app.controller('MainCtrl', [
-'$scope',
-function($scope){
-  $scope.test = 'Hello world!';
-}]);
 
